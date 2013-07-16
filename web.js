6a01d36bbcdf2e8a9e6a1afd2df4a2071b5cf581
Balaji Srinivasan <kevinsikariam@gmail.com>
2013-07-16 05:24:20 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var fs = require('fs');
+buf = new Buffer(256);
+
+
+app.get('/', function(request, response)
+{
+  response.send(console.log(buf.toString(fs.readFile(index.html))));
 });
 
 var port = process.env.PORT || 5000;
