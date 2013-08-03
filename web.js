6a382c5c6340de6d617adfa42fc1e1e901b4823d
Ubuntu <ubuntu@ip-172-31-42-138.us-west-2.compute.internal>
2013-08-03 01:28:28 +0000

    Edited product desc in dex.html

--- web.js
+++ web.js
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
 
 var express = require('express');
-var app = express.createServer(express.logger());
 var fs = require('fs');
 var filename = "index.html";
-var buf = new Buffer(fs.readFileSync(filename));
-var out;
 
-buf = fs.readFileSync(filename);
-out = buf.toString();
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response)
 {
-  response.send(out);
+  var html = fs.readFileSync(filename).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
